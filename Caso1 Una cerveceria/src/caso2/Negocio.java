6de8c7cde5e6a25393281c552fd8e46ea6f704a5
JuanMaMujica <50682533+JuanMaMujica@users.noreply.github.com>
2021-09-06 18:35:58 -0300

    Terminado caso2
    ProgramacionDefensiva terminada en capa modelo y capa negocio. Validaciones. Excepciones

--- Caso1 Una cerveceria/src/caso2/Negocio.java
+++ Caso1 Una cerveceria/src/caso2/Negocio.java
@@ -1,36 +1,91 @@
 package caso2;
 
+import excepciones.NoExisteSurtidorException;
+import excepciones.SurtidorVacioException;
+
+/**
+ * @author Agüero Sebastián, Mujica Juan Manuel, Navarro Pablo, Vucetic Ivo
+
+ *
+ */
 public class Negocio {
 	
+	private Surtidor surtidor = null;
 	
-	public void InicializaSurtidor(double carga) {
-		Surtidor.getInstanceSurtidor().InicializarSurtidor(carga);
+	public Negocio () {
+		this.surtidor = Surtidor.getInstanceSurtidor();
+	}
+	
+	/**Metodo que se encarga de inicializar los valores del surtidor<br>
+	 * <b>Pre:</b>El parametro carga debe ser mayor a 0 y menor o igual a 2000<br> 
+	 * @param carga parametro que representa la cantidad de litros con que se incializa el surtidor
+	 * @throws NoExisteSurtidorException excepcion lanzada si no existe el surtidor
+	 */
+	public void InicializaSurtidor(double carga) throws NoExisteSurtidorException{
+		assert carga > 0 : "La carga debe ser mayor a 0";
+		assert carga <= 2000: "La carga debe ser menor o igual a 2000";
+		if(this.getExistenciaDeposito()) 
+			Surtidor.getInstanceSurtidor().InicializarSurtidor(carga);
+		else 
+			throw new NoExisteSurtidorException("No existe surtidor");
 	}
 		
+	/** Metodo que le indica a la capa modelo que cuantos litros debe cargar al surtidor <br>
+	 * <b>Pre:</b> El parametro carga debe ser mayor a 0 y menor o igual a 2000 <br>
+	 * @param carga parametro que representa la cantidad de litros que se va a cargar en el surtidor
+	 */
 	public void cargaSurtidor(double carga) {
+		assert carga>0:"La carga debe ser mayo a 0";
+		assert carga<=2000:"La carga debe ser menor o igual a 2000";
 		Surtidor.getInstanceSurtidor().cargarSurtidor(carga);
 	}
-	// ----------------------------------------------------------------------
-	public void activaManguera1() {
-		Surtidor.getInstanceSurtidor().descargaManguera1();
+
+	/** Metodo que se encarga de comunicar a la capa modelo que comience la descarga de combustible en la manguera 1
+	 * @param carga parametro que representa el combustible que quiere cargar el usuario
+	 * @throws SurtidorVacioException pasa la excepcion arrojada en modela a la ui
+	 */
+	public void activaManguera1(double carga) throws SurtidorVacioException {
+		try {
+			Surtidor.getInstanceSurtidor().descargaManguera1(carga);
+		} catch (SurtidorVacioException e) {
+			// TODO Auto-generated catch block
+			throw e;
+		}
 	}
 	
-	public void activaManguera2() {
-		Surtidor.getInstanceSurtidor().descargaManguera2();
+	/** Metodo que se encarga de comunicar a la capa modelo que comience la descarga de combustible en la manguera 2
+	 * @param carga parametro que representa el combustible que quiere cargar el usuario
+	 * @throws SurtidorVacioException pasa la excepcion arrojada en modela a la ui
+	 */
+	public void activaManguera2(double carga) throws SurtidorVacioException {
+		try {
+			Surtidor.getInstanceSurtidor().descargaManguera2(carga);
+		} catch (SurtidorVacioException e) {
+			// TODO Auto-generated catch block
+			throw e;
+		}
 	}
 	
+	/**Metodo que se encarga de informar a la capa modelo que desactive la manguera 1
+	 * 
+	 */
 	public void desactivaMaguera1() {
-		
+		Surtidor.getInstanceSurtidor().setManguera1Activada(false);
 	}
 	
+	/**Metodo que se encarga de informar a la capa modelo que desactive la manguera 2
+	 * 
+	 */
 	public void desactivaMaguera2() {
-		
+		Surtidor.getInstanceSurtidor().setManguera2Activada(false);
 	}
 	
 	
-	//------------------------------------------------------------------
 	public boolean getExistenciaDeposito() {
-		return Surtidor.getInstanceSurtidor().getExistenciaDeposito();
+		if(Surtidor.instance == null)
+			return false;
+		else
+			return true;
 	}
 	
 	public double getAcumuladoManguera1() {
--- Caso1 Una cerveceria/src/caso2/Surtidor.java
+++ Caso1 Una cerveceria/src/caso2/Surtidor.java
@@ -1,34 +1,30 @@
 package caso2;
 
+import excepciones.CargaImposibleException;
 import excepciones.SurtidorVacioException;
 
 /**
  * @author Agüero Sebastián, Mujica Juan Manuel, Navarro Pablo, Vucetic Ivo
- * Invariante de clase: manguera1 y manguera2 deben ser siempre distinto de null
  * 
- *
- */
-/**
- * @author rochy
+ * 
  *
  */
 public class Surtidor {
 
 	public static Surtidor instance = null;
 	private double cantidad_Combustible;
-	private double acumulado_Venta;
 	private double acumuladoManguera1;
 	private double acumuladoManguera2;
 	private double ultimaventaManguera1;
 	private double ultimaventaManguera2;
-	private Manguera manguera1;
-	private Manguera manguera2;
+	private boolean manguera1Activada;
+	private boolean manguera2Activada;
 	private static double precio_Litro=80;
 	
 	private Surtidor() {
 		super();
-		manguera1 = new Manguera();
-		manguera2 = new Manguera();
+		manguera1Activada = false;
+		manguera2Activada = false;
 	}
 	
 	public static Surtidor getInstanceSurtidor() {
@@ -43,66 +39,81 @@ public class Surtidor {
 	 * <b> Pos:</b> La variable cantidad_Combustible debe ser menor o igual a 2000<br>
 	 * @param carga parametro que resprensenta la cantidad inicial de combustible. 
 	 */
-	public void InicializarSurtidor(double carga) {
+	public void InicializarSurtidor(double carga){
 		assert this.cantidad_Combustible==0:"El surtidor no debe contener combustible";
-		verificaInvariante();
-		if(carga >=0 && carga<=2000) {
+		if(carga >=1 && carga<=2000) {
 			this.cantidad_Combustible = carga;
-			this.acumulado_Venta = 0;
 			this.acumuladoManguera1 = 0;
 			this.acumuladoManguera2 = 0;
 			this.ultimaventaManguera1 = 0;
 			this.ultimaventaManguera2 = 0;	
-		}
-		verificaInvariante();
 		assert this.cantidad_Combustible<=2000:"La cantidad de combustible debe ser menor o igual a 2000";
 	}
 	
 	/** Metodo que se encarga de la recarga del surtidor
-	 * <b> Pre: </b> la variable cantidad_Combustible debe ser >=0 <br>
+	 * <b> Pre: </b> la variable cantidad_Combustible debe ser >=0<br>
 	 * <b> Pos: </b> la variable cantidad_Combustible debe ser menor o igual a 2000 <br>
 	 * @param carga parametro que representa la cantidad de combustible que se le carga al surtidor<br>
 	 */
 	public void cargarSurtidor(double carga) {
 		assert cantidad_Combustible>=0: "La cantidad de combustible en el surtidor debe ser mayor o igual a 0";
-		verificaInvariante();
 		if(carga>=0 && carga<=2000) {
 			this.cantidad_Combustible += carga;
 		}
-		verificaInvariante();
 		assert cantidad_Combustible<=2000:"La cantidad de combustible en el surtidor debe ser menor o igual a 2000";
 	}
 	
-	public void descargaManguera1() throws SurtidorVacioException{
-		
+	/** Metodo que se encarga de comenzar la descarga de combustible de la manguera 1 <br>
+	 * <b>Pre:</b> El parametro carga debe ser mayor a 0 y menor o igual a 2000. La variable cantidad_Combustible debe ser mayor o igual a 0 y menor o igual a 2000<br>
+	 * <b>Pos:</b> La variable cantidad_Combustible debe ser mayor o igual a 0<br>
+	 * @param carga parametro que representa lo que el ususario desea cargar
+	 * @throws SurtidorVacioException excepcion que se lanza si el surtidor esta vacio
+	 */
+	public void descargaManguera1(double carga) throws SurtidorVacioException{
+		assert carga>0: "La carga debe ser mayor a 0";
+		assert carga<=2000: "La carga debe ser menor o igual a 2000";
+		assert cantidad_Combustible>=0:"La cantidad de combustible en el surtidor debe ser mayor o igual a 0";
+		assert cantidad_Combustible<=2000:"La cantidad del combustible debe ser menor o igual a 2000";
 		double cantidad_extraida = 0;
 		
 		if(this.cantidad_Combustible>0) {
-			while(this.cantidad_Combustible >= 0) {
+			while(this.cantidad_Combustible >= 0 || carga <= cantidad_extraida) {
 				this.cantidad_Combustible -= 1;
 				cantidad_extraida += 1;
-				this.acumuladoManguera2 += 1;
-				//wait de 1 segundo
+				this.acumuladoManguera1 += 1;
 			}
 		}
 		else 
-			throw new SurtidorVacioException("El surtidor esta vacio");
+			throw new SurtidorVacioException("Surtidor Vacio");
+		this.ultimaventaManguera1 = cantidad_extraida * Surtidor.precio_Litro;
+		assert cantidad_Combustible>=0:"La cantidad de combustible debe ser mayor o igual a 0";
 	}
 	
-	public void descargaManguera2() throws SurtidorVacioException{
-		//va con una excepcion para saber si se puede descargar o no
+	/** Metodo que se encarga de comenzar la descarga de combustible de la manguera 2 <br>
+	 * <b>Pre:</b> El parametro carga debe ser mayor a 0 y menor o igual a 2000. La variable cantidad_Combustible debe ser mayor o igual a 0 y menor o igual a 2000<br>
+	 * <b>Pos:</b> La variable cantidad_Combustible debe ser mayor o igual a 0<br>
+	 * @param carga parametro que representa lo que el ususario desea cargar
+	 * @throws SurtidorVacioException excepcion que se lanza si el surtidor esta vacio
+	 */
+	
+	public void descargaManguera2(double carga) throws SurtidorVacioException{
+		assert carga>0: "La carga debe ser mayor a 0";
+		assert carga<=2000: "La carga debe ser menor o igual a 2000";
+		assert cantidad_Combustible>=0:"La cantidad de combustible en el surtidor debe ser mayor o igual a 0";
+		assert cantidad_Combustible<=2000:"La cantidad del combustible debe ser menor o igual a 2000";
 		double cantidad_extraida = 0;
 		
 		if(this.cantidad_Combustible>0) {
-			while(this.cantidad_Combustible >= 1) {
+			while(this.cantidad_Combustible >= 0 || carga == cantidad_extraida) {
 				this.cantidad_Combustible -= 1;
 				cantidad_extraida += 1;
 				this.acumuladoManguera2 += 1;
-				//wait de 1 segundo
 			}
 		}
 		else 
-			throw new SurtidorVacioException("El surtidor esta vacio");
+			throw new SurtidorVacioException("Surtidor Vacio");
+		this.ultimaventaManguera2 = cantidad_extraida * Surtidor.precio_Litro;
+		assert cantidad_Combustible>=0:"La cantidad de combustible debe ser mayor o igual a 0";
 	}
 	
 	/** Metodo que ser encarga de devolver true o false segun si existe el surtidor<br>
@@ -118,7 +129,6 @@ public class Surtidor {
 	 */
 	public double getAcumuladoManguera1(){
 		assert this.acumuladoManguera1 >= 0: "El combustible descargado por la manguera 1 debe ser mayor o igual a 0";
-		this.verificaInvariante();
 		return this.acumuladoManguera1;
 	}
 	
@@ -128,7 +138,6 @@ public class Surtidor {
 	 */
 	public double getAcumuladoManguera2(){
 		assert this.acumuladoManguera2 >= 0:"El combustible descargado por la manguera 2 debe ser mayor o igual a 0";
-		this.verificaInvariante();
 		return this.acumuladoManguera2;
 	}
 	
@@ -139,7 +148,6 @@ public class Surtidor {
 	 */
 	public double getUltimaVentaMG1() {
 		assert this.ultimaventaManguera1>=0:"El valor de la ultima venta debe ser mayor o igual a 0";
-		this.verificaInvariante();
 		return this.ultimaventaManguera1;
 	}
 
@@ -149,12 +157,18 @@ public class Surtidor {
 	 */
 	public double getUltimaVentaMG2() {
 		assert this.ultimaventaManguera2>=0:"El valor de la ultima venta debe ser mayor o igual a 0";
-		this.verificaInvariante();
 		return this.ultimaventaManguera2;
 	}
 	
-	public void verificaInvariante() {
-		assert this.manguera1 != null: "El atributo manguera1 no debe ser null";
-		assert this.manguera2 != null: "El atributo manguera2 no debe ser null";
+	public void setManguera1Activada(boolean manguera1Activada) {
+		this.manguera1Activada = manguera1Activada;
 	}
+
+	public void setManguera2Activada(boolean manguera2Activada) {
+		this.manguera2Activada = manguera2Activada;
+	}
+
+	
+
+	
 }
