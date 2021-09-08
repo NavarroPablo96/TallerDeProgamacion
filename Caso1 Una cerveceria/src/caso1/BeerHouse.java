744b4c0f1fa343cc5926f6c51c798abd5d45d35b
NavarroPablo96 <89087992+NavarroPablo96@users.noreply.github.com>
2021-09-08 20:08:58 -0300

    fin caso 1
    al fin caso 1

--- Caso1 Una cerveceria/src/caso1/BeerHouse.java
+++ Caso1 Una cerveceria/src/caso1/BeerHouse.java
@@ -1,11 +1,11 @@
 package caso1;
 /**
- * @author Agüero Sebastián, Mujica Juan Manuel, Navarro Pablo, Vucetic Ivo
- * <br>
+ * @author Agüero Sebastián, Mujica Juan Manuel, Navarro Pablo, Vucetic Ivo<br>
+ * Invariante de clase los atributos cantidadDeMesasHabilitadas y cantidadDeProductos deben ser positivos<br>
+ * El atributo mesas y carta debe ser distinto de null<br>
  * Clase BeerHouse 
  * */
 import java.util.HashMap;
-
 import excepciones.MesaNoDisponibleException;
 
 public class BeerHouse {
@@ -13,36 +13,20 @@ public class BeerHouse {
 	private int cantidadDeMesasHabilitadas;
 	private int cantidadDeProductos;
 	private HashMap<Integer, Mesa> mesas = new HashMap<Integer,Mesa>();
+	private HashMap<Integer, Producto> carta = new HashMap<Integer,Producto>();
+	
 	private boolean open=false;
 	
-	
-	public HashMap<Integer, Mesa> getMesas() {
-		return mesas;
-	}
-
-	public boolean isOpen() {
-		return this.open;
-	}
-
-	public int getCantMesasMaxima() {
-		return this.mesas.size();
-	}
-
+	/**
+	 * Constructor. Se inicializan algunos valores iniciales en cero
+	 * <br>
+	 * */
 	public BeerHouse() {
 		this.cantidadDeMesasHabilitadas=0;
 		this.cantidadDeProductos=0;
 	}
-	
-	
-	private void desocuparMesas() {
-		Mesa actual=null;
-		for(int i=0 ; i<=this.mesas.size(); i++) {
-			actual=this.mesas.get(i);
-			actual.setOcupada(false);
-		}
-	}
-	
-	
+
+	//esto es para un assert
 	private boolean verificaMesasVacias() {
 		Mesa actual=null;
 		boolean cumpleLibres=true;
@@ -56,63 +40,109 @@ public class BeerHouse {
 		return cumpleLibres;
 	}
 	
-	
 	/**
+	 * Metodo abrirLocal <br>
 	 * <b>Pre: </b> parámetro cantMesas debe ser positivo<br>
-	 * <b>Post: </b> todas las mesas estan desocupadas <br>
+	 * El atributo mesas y carta debe ser distinto de null<br>
+	 * <b>Post: </b> Se abrió el negocio <br>
+	 * todas las mesas estan desocupadas <br>
 	 * la carta esta actualizada con todos los productos disponibles <br>
-	 */
+	 * */
 	public void abrirLocal(int cantMesas) {
-		verificaInvariante();
+		assert this.mesas!=null :"El atributo mesas es null, de la clase BeerHouse";
+		assert this.carta!=null :"El atributo carta es null, de la clase BeerHouse";
 		assert cantMesas>0 : "BeerHouse.abrirLocal() cantMesas no es positiva";
 		
-		desocuparMesas();
-		this.cantidadDeProductos=0;
+		//desocupar mesas
+		Mesa actual=null;
+		for(int i=0 ; i<=cantMesas; i++) {
+			actual= new Mesa();
+			this.mesas.put(actual.getNumero(),actual);
+			actual.setOcupada(false);
+		}
+		
+		//cargar productos
+		this.carta.put(1, new Producto(140,1, "Pinta de Cerveza negra"));
+		this.carta.put(2, new Producto(100,2, "Pinta de Cerveza rubia"));
+		this.carta.put(3, new Producto(350,3, "Muzzarella "));
+		this.carta.put(4, new Producto(100,4, "Gaseosa"));
+		this.carta.put(5, new Producto(90,5, "agua"));
+		
+		
+		this.cantidadDeProductos=5;
 		this.cantidadDeMesasHabilitadas=cantMesas;
 		this.open=true;
 		
 		assert verificaMesasVacias() : "BeerHouse.abrirLocal() Las mesas no están vacias";
+		assert this.open=true :"El local no se abrió";
 		verificaInvariante();
 	}
 	
-	public void agregarMesa() {
-		Mesa mesa= new Mesa();
-		
-		this.mesas.put(mesa.getNumero(),mesa);
-		
-	}
 	
+	/**
+	 * Metodo ocuparMesa <br>
+	 * <b>Pre: </b> el parámetro nroMesa debe ser positivo<br>
+	 * <b>Post: </b> Se retorna una referencia no nula de la Mesa dada por el parámetro nroMesa<br>
+	 * Se ocupa la mesa recibida por parámetro<br>
+	 * @throws MesaNoDisponibleException en caso de intentar ocupar una mesa ya ocupada<br>
+	 * */
 	public Mesa ocuparMesa(int nroMesa)throws MesaNoDisponibleException {
-		Mesa aux;
 		verificaInvariante();
+		assert nroMesa>0 : "BeerHouse.ocuparMesa() nroMesa no es positivo";
 		
-		//¿Deberia este metodo verificar si el local está abierto?
-		//podría ser un invariante
+		Mesa aux;
 		aux=this.mesas.get(nroMesa);
 		if(aux.isOcupada())
 			throw new MesaNoDisponibleException("La mesa ya está ocupada");
 		aux.setOcupada(true);
 		
+		assert aux!=null : "BeerHouse.ocuparMesa() aux es null";
+		assert aux.isOcupada(): "BeerHouse.ocuparMesa() No se ocupo la mesa";
 		verificaInvariante();
 		return aux;
 	}
 	
+	
+	
+	/**
+	 * Metodo cerrarMesa <br>
+	 * <b>Pre: </b> el parámetro nroMesa debe ser positivo<br>
+	 * el parámetro nroMesa debe ser menor al atributo cantidad de mesas habilitadas<br>
+	 * <b>Post: </b> Se cerro la mesa, se retorno un importe positivo<br>
+	 * */
 	public double cerrarMesa(int nroMesa) {
 		verificaInvariante();
+		assert nroMesa>0 && nroMesa<=this.cantidadDeMesasHabilitadas : "BeerHouse.cerrarMesa() nroMesa es incorrecto";
 		
 		double aux=this.mesas.get(nroMesa).getImporte();
 		this.mesas.get(nroMesa).setOcupada(false);
 		this.mesas.get(nroMesa).setImporte(0);
 		
+		assert this.mesas.get(nroMesa).isOcupada() == false : "BeerHouse.cerrarMesa() No se cerro la mesa";
+		assert aux>0 : "BeerHouse.cerrarMesa() aux no es positivo";
 		verificaInvariante();
 		return aux;
 	}
 	
 	private void verificaInvariante() {
-		assert cantidadDeMesasHabilitadas>=1 : "No cumple invariante de clase"; 
-		assert cantidadDeProductos>=1 :  "No cumple invariante de clase";
+		assert cantidadDeMesasHabilitadas>=1 : "La cantidad de Mesas Habilitadas no es positiva";
+		assert cantidadDeProductos>=1 :  "La cantidad de productos no es positiva";
+		assert this.mesas!=null :"El atributo mesas es null, de la clase BeerHouse";
+		assert this.carta!=null :"El atributo carta es null, de la clase BeerHouse";
+	}
+	
+	public HashMap<Integer, Mesa> getMesas() {
+		return mesas;
 	}
 
+	public boolean isOpen() {
+		return this.open;
+	}
+
+	public int getCantMesasMaxima() {
+		return this.mesas.size();
+	}
+	
 	public int getCantidadDeMesasHabilitadas() {
 		return cantidadDeMesasHabilitadas;
 	}
--- Caso1 Una cerveceria/src/caso1/Negocio.java
+++ Caso1 Una cerveceria/src/caso1/Negocio.java
@@ -1,64 +1,101 @@
 package caso1;
+
 import excepciones.CantMesasIncorrectoException;
 import excepciones.MesaNoDisponibleException;
 
 /**
  * @author Agüero Sebastián, Mujica Juan Manuel, Navarro Pablo, Vucetic Ivo
  * <br>
+ * Invariante de clase el atributo cerveceria distinto de null
  * Clase Negocio
  * */
 public class Negocio {
 	
 	private BeerHouse cerveceria=null;
 	
+	/**
+	 * Constructor. Se crea una instancia de tipo BeerHouse
+	 * <br>
+	 * */
 	public Negocio() {
 		this.cerveceria=new BeerHouse();
+		
+		verificaInvariante();
 	}
 	
 	public BeerHouse getCerveceria() {
+		verificaInvariante();
+		
 		return cerveceria;
 	}
 
+	private void verificaInvariante() {
+		assert this.cerveceria!=null : "El atributo cerveceria de la clase Negocio es null";
+		
+	}
 	
 	/**
 	 * Metodo abrirLocal <br>
 	 * Delega en la capa de modelo su tarea <br>
 	 * Valida los parametros y los pasa a la capa de modelo <br>
-	 * Si sucede algún excepción se muestra <br>
 	 * 
-	 * <b>Post: </b> Se abrio el negocio <br>
-	 * @throws 
+	 * <b>Post: </b> Se abrió el negocio <br>
+	 * @throws CantMesasIncorrectoException en caso de intentar con un numero incorrecto de mesa<br>
 	 * */
 	public void abrirLocal(int cantMesas)throws CantMesasIncorrectoException  {
-		//cantMesas no está validado
+		verificaInvariante();
+		
 		if(cantMesas<=0 || cantMesas>this.cerveceria.getCantMesasMaxima())
 			throw new CantMesasIncorrectoException("Negocio.abrirLocal() necesita 0<cantMesas<=CantMáxima");
 		
 		this.cerveceria.abrirLocal(cantMesas);
-		//postCondición se debe abrir el local
+
+		assert this.cerveceria.isOpen(): "El local no se abrió";
+		verificaInvariante();
 	}
 	
+	/**
+	 * Metodo ocuparMesa <br>
+	 * Delega en la capa de modelo su tarea <br>
+	 * valida los parámetros y los pasa a la capa de modelo <br>
+	 * <b>Post: </b> Se retorna una referencia no nula de la Mesa dada por el parámetro nroMesa<br>
+	 * @throws CantMesasIncorrectoException en caso de intentar con un numero incorrecto de mesa<br>
+	 * @throws MesaNoDisponibleException en caso de intentar ocupar una mesa ya ocupada<br>
+	 * */
 	public Mesa ocuparMesa(int nroMesa) throws CantMesasIncorrectoException,MesaNoDisponibleException{
+		verificaInvariante();
+		
 		Mesa act=null;
-		//se debe validad nroMesa
 		if(nroMesa<=0 || nroMesa>this.cerveceria.getCantidadDeMesasHabilitadas())
 			throw new CantMesasIncorrectoException("Negocio.ocuparMesa() necesita 0<nroMesa<=CantMesasHabilitadas");
 		
 		act= this.cerveceria.ocuparMesa(nroMesa);
-		
-		//post Condicion:
+
 		assert act!=null : "Negocio.ocuparMesa() la mesa no fue creada";
+		verificaInvariante();
 		return act;
 	}
 	
+	/**
+	 * Metodo cerrarMesa <br>
+	 * Delega en la capa de modelo su tarea <br>
+	 * Valida los parámetros y los pasa a la capa de modelo <br>
+	 * 
+	 * <b>Post: </b> Se cerro la mesa, se retorno un importe positivo<br>
+	 * @throws CantMesasIncorrectoException en caso de intentar con un numero incorrecto de mesa<br>
+	 * */
 	public double cerrarMesa(int nroMesa)throws CantMesasIncorrectoException {
-		//se debe validar nroMesa
+		verificaInvariante();
+		
+		
 		if(nroMesa<=0 || nroMesa>this.cerveceria.getCantidadDeMesasHabilitadas())
 			throw new CantMesasIncorrectoException("Negocio.ocuparMesa() necesita 0<nroMesa<=CantMesasHabilitadas");
 		
 		double act= this.cerveceria.cerrarMesa(nroMesa);
 		
+		
 		assert act>=0 : "Negocio.ocuparMesa() importe total negativo";
+		verificaInvariante();
 		return act;
 		
 	}
